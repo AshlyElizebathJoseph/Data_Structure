c044347639de06e7f34c989c60a8d6a3d8cfc655
Ashly Elizebath Joseph <108012500+AshlyElizebathJoseph@users.noreply.github.com>
2024-11-08 20:09:25 +0530

    Create Merge_Sort.py

---
+++ Merge_Sort.py
@@ -0,0 +1,39 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+def get_user_input():
+    user_input = input("Enter the numbers to be sorted, separated by spaces: ")
+    arr = list(map(int, user_input.split()))
+    return arr
+
+array = get_user_input()
+print("Unsorted array:", array)
+
+merge_sort(array)
+print("Sorted array:", array)
+
